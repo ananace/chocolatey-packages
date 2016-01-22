dec1a23d0976b07f39193aa193205bde0de0c163
Alexander "Ace" Olofsson <ace@haxalot.com>
2016-01-22 22:22:47 +0100

    OpenMW 0.38.0 released

---
+++ Packages/OpenMW/0.38.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'openmw'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = 'https://github.com/OpenMW/openmw/releases/download/openmw-0.38.0/OpenMW-0.38.0-win32.exe'
+  url64bit      = 'https://github.com/OpenMW/openmw/releases/download/openmw-0.38.0/OpenMW-0.38.0-win64.exe'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+
+  registryUninstallerKey = 'OpenMW 0.38.0'
+}
+
+Install-ChocolateyPackage @packageArgs
