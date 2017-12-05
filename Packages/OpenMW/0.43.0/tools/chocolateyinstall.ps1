82ac4e0fc2bcff34e411f22c97787f2ee1389b85
Alexander "Ace" Olofsson <ace@haxalot.com>
2017-12-05 17:28:34 +0100

    Release OpenMW 0.43.0

---
+++ Packages/OpenMW/0.43.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'openmw'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'exe'
+  url            = 'https://github.com/OpenMW/openmw/releases/download/openmw-0.43.0/OpenMW-0.43.0-win32.exe'
+  url64bit       = 'https://github.com/OpenMW/openmw/releases/download/openmw-0.43.0/OpenMW-0.43.0-win64.exe'
+
+  checksum       = '2e807fbded5f1ca766afa0eea00d9ff1cf2f102d888e47636872dd859882bf0d'
+  checksumtype   = 'sha256'
+  checksum64     = '7d3648ccc536ed91759d09bbe37aa9f660988bb251ba8e9d39a6ccda6cb87a94'
+  checksumtype64 = 'sha256'
+
+  silentArgs     = "/S"
+  validExitCodes = @(0)
+
+  registryUninstallerKey = 'OpenMW 0.43.0'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ Packages/OpenMW/0.43.0/tools/chocolateyuninstall.ps1
@@ -0,0 +1,31 @@
+﻿$packageName = 'openmw'
+$registryUninstallerKeyName = 'OpenMW 0.43.0' #ensure this is the value in the registry
+$shouldUninstall = $true
+
+$local_key     = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$local_key6432   = "HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName" 
+$machine_key   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$machine_key6432 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+
+$file = @($local_key, $local_key6432, $machine_key, $machine_key6432) `
+    | ?{ Test-Path $_ } `
+    | Get-ItemProperty `
+    | Select-Object -ExpandProperty UninstallString
+
+if ($file -eq $null -or $file -eq '') {
+    Write-Host "$packageName has already been uninstalled by other means."
+    $shouldUninstall = $false
+}
+
+$installerType = 'EXE' 
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+if (!(Test-Path $file)) {
+    Write-Host "$packageName has already been uninstalled by other means."
+    $shouldUninstall = $false
+}
+
+if ($shouldUninstall) {
+ Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+}
