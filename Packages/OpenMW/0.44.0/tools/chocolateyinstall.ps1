e1f8279666cf85dc8492d852939215d8df5af4c6
unknown <ace@haxalot.com>
2019-03-28 16:19:35 +0100

    Add 0.44.0, 0.45.0

---
+++ Packages/OpenMW/0.44.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'openmw'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'exe'
+  url            = 'https://github.com/OpenMW/openmw/releases/download/openmw-0.44.0/OpenMW-0.44.0-win32.exe'
+  url64bit       = 'https://github.com/OpenMW/openmw/releases/download/openmw-0.44.0/OpenMW-0.44.0-win64.exe'
+
+  checksum       = '64dd58555fed148294227d686fd927cf5b3201814a30922a5e85515fde6e0aff'
+  checksumtype   = 'sha256'
+  checksum64     = '35ff64c45334b4a76bdb2c85c041be3a2b7b4de0e75e673378d97847e7374e99'
+  checksumtype64 = 'sha256'
+
+  silentArgs     = "/S"
+  validExitCodes = @(0)
+
+  registryUninstallerKey = 'OpenMW 0.44.0'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ Packages/OpenMW/0.45.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'openmw'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'exe'
+  url            = 'https://github.com/OpenMW/openmw/releases/download/openmw-0.45.0/OpenMW-0.45.0-win32.exe'
+  url64bit       = 'https://github.com/OpenMW/openmw/releases/download/openmw-0.45.0/OpenMW-0.45.0-win64.exe'
+
+  checksum       = '45035b1d94ace4b36f2ba9907d9702debf615b97acbc78a9c550c2d29da74b5a'
+  checksumtype   = 'sha256'
+  checksum64     = 'a5aec5817973338d621c82e2a4a3f9c900e69ae433c47f5fe6cb621c40dab523'
+  checksumtype64 = 'sha256'
+
+  silentArgs     = "/S"
+  validExitCodes = @(0)
+
+  registryUninstallerKey = 'OpenMW 0.45.0'
+}
+
+Install-ChocolateyPackage @packageArgs
