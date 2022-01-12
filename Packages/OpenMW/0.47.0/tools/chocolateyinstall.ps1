f313b8ca47ab6bf651bc289d88ac089da32b6068
Alexander Olofsson <ace@haxalot.com>
2022-01-12 16:28:17 +0100

    Add OpenMW 0.47.0

---
+++ Packages/OpenMW/0.47.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'openmw'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'exe'
+  url64bit       = 'https://github.com/OpenMW/openmw/releases/download/openmw-0.47.0/OpenMW-0.47.0-win64.exe'
+
+  checksum64     = '02702977ae32d73aa20e5710a20bc5452d7131dab63bc43d194e339cadaf1e61'
+  checksumtype64 = 'sha256'
+
+  silentArgs     = "/S"
+  validExitCodes = @(0)
+
+  registryUninstallerKey = 'OpenMW 0.47.0'
+}
+
+Install-ChocolateyPackage @packageArgs
