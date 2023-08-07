f327d4883ed9f7f4ad3c84670f6d7206e51c3928
Alexander Olofsson <ace@haxalot.com>
2023-08-07 18:51:57 +0200

    Add OpenMW 0.48.0

---
+++ Packages/OpenMW/0.48.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'openmw'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'exe'
+  url64bit       = 'https://github.com/OpenMW/openmw/releases/download/openmw-0.48.0/OpenMW-0.48.0-win64.exe'
+
+  checksum64     = 'bc2522e48a1bae8fe9ed24994827f09dc95f74d3e6dfe4236cac07c9cdd3580f'
+  checksumtype64 = 'sha256'
+
+  silentArgs     = "/S"
+  validExitCodes = @(0)
+
+  registryUninstallerKey = 'OpenMW 0.48.0'
+}
+
+Install-ChocolateyPackage @packageArgs
